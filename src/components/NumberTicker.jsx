573f24209c4d6b05af5bfc2769c99c52f1533bc1
unknown <2112026@nec.edu.in>
2025-02-01 09:31:53 +0530

    Updated Number

--- src/components/NumberTicker.jsx
+++ src/components/NumberTicker.jsx
@@ -12,7 +12,7 @@ const NumberCounter = () => {
             <div className="flex items-baseline font-orbitron">
               <NumberTicker
                 className="text-lg md:text-4xl font-bold bg-gradient-to-r from-purple-400 to-pink-400 text-transparent bg-clip-text"
-                value={70}
+                value={110}
                 duration={2000}
               />
               <span className="text-lg md:text-4xl font-bold bg-gradient-to-r from-purple-400 to-pink-400 text-transparent bg-clip-text">+</span>
@@ -40,15 +40,15 @@ const NumberCounter = () => {
         <div className="stat-card flex-1 transform hover:-translate-y-2 hover:scale-105 transition-all duration-300 ease-out cursor-pointer">
           <div className="flex flex-col items-center p-2 rounded-lg bg-white/5 hover:bg-white/10 transition-colors shadow-lg hover:shadow-purple-500/20">
             <div className="flex items-baseline font-orbitron">
-              <span className="text-lg md:text-4xl font-bold bg-gradient-to-r from-purple-400 to-pink-400 text-transparent bg-clip-text">0</span>
+              <span className="text-lg md:text-4xl font-bold bg-gradient-to-r from-purple-400 to-pink-400 text-transparent bg-clip-text"></span>
               <NumberTicker
                 className="text-lg md:text-4xl font-bold bg-gradient-to-r from-purple-400 to-pink-400 text-transparent bg-clip-text"
-                value={8}
+                value={10}
                 duration={3000}
               />
               <span className="text-lg md:text-4xl font-bold bg-gradient-to-r from-purple-400 to-pink-400 text-transparent bg-clip-text">+</span>
             </div>
-            <span className="text-purple-200 text-[10px] md:text-base mt-1 font-space-grotesk tracking-wider">Hackathons</span>
+            <span className="text-purple-200 text-[10px] md:text-base mt-1 font-space-grotesk tracking-wider">Workshops</span>
           </div>
         </div>
       </div>
